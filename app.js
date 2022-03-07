c51f9817ee9dae6554ccb3f231a10a35234c7a74
theodore_stamatiadis <stamatiadisteo@gmail.com>
2022-03-07 22:30:25 +0200

    some fixes in responsiveness and added typewriter

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const spans = document.querySelectorAll(".intro-info span");
 const cards = document.querySelectorAll(".card");
 const cardMessage = document.querySelector(".message");
+const typewriter = document.querySelector(".typewrite");
 
 const transit = (i) => {
     setTimeout(function() {
@@ -8,18 +9,33 @@ const transit = (i) => {
     },400*i);
 }
 
-
 spans.forEach((span,index)=>transit(index))
 
-cards.forEach(c=> {
-    c.addEventListener("click",cardRotate);
+document.addEventListener("scroll",function() {
+    const isIn = isInViewport(typewriter);
+    if(isIn && !typewriter.classList.contains("start-anim")) typewriter.classList.add("start-anim");
+})
+
+cards.forEach(card=> {
+    card.addEventListener("click",cardRotate);
 });
 
 cardMessage.addEventListener("click",cardRotate);
 
 
+
 function cardRotate() {
     cardMessage.classList.add("hide-message");
     return (this==cards[0] || this==cardMessage) ? cards[0].classList.toggle("active-rotate"):this.classList.toggle("active-rotate");
+}
+
+function isInViewport(elem) {
+    const rect = elem.getBoundingClientRect();
+
+    return (
+        rect.top >=0 && 
+        rect.left >= 0 && 
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
 }
 
